0958fee0850418f753a6bb8784373b6ae6f8ee87
Simon Lipke <simon.lipke@googlemail.com>
2016-10-12 16:04:48 +0200

    [TASK] Migrated to existing GUI

---
+++ app/src/main/java/de/hdm_stuttgart/zeitfresser/EvalActivity.java
@@ -0,0 +1,78 @@
+package de.hdm_stuttgart.zeitfresser;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+
+/**
+ * EvalActivity ist die Aktivität, diebeim Klick auf den Eintrag
+ * "Evaluation" im Drawer-Menü aufgerufen wird
+ */
+public class EvalActivity extends CommonActivity {
+
+    /**
+     * diese Methode muss nicht verändret werden, sie baut das Kuchendiagramm auf
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eval);
+        createNavigationBars();
+
+        //Content of the pie chart
+        PieChart pieChart = (PieChart) findViewById(R.id.chart);
+
+        PieDataSet dataset = new PieDataSet(getEntries(), "Time spent");
+
+        //Set the data
+        PieData data = new PieData(getLabels(), dataset); // initialize Piedata
+        pieChart.setData(data); //set data into chart
+
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); // set the color
+        dataset.setValueTextSize(16);
+
+    }
+
+    /**
+     * TODO: diese Methode muss anwendungsspezifisch überschrieben werden
+     * Sie liefert eine Liste von Zahlen, die im Kuchendiagramm angezeigt werden
+     * Die Zahlen werden in einem Entry-Objekt gespeichert: es enthält die darzustellende
+     * Zahl sowie einen eindeutigen Index. Der Index dient zur beschreibung der Reihenfolge
+     * der Entries
+     * In der Anwendung entsprechen die Zaheln der Dauern, die im Kuchendiagramm angezeigt werden
+     * sollen
+     */
+    private ArrayList<Entry> getEntries() {
+        // creating data values
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+
+        return entries;
+    }
+
+    /**
+     * TODO: diese Methode muss anwendungsspezifisch überschrieben werden
+     * Sie liefert eine Liste von Labels, mit denen die Zahlen aus der  Methode "getEntries"
+     * im Kuchendiagramm beschriftet werden. Die Reihenfolge der Labels muss zu der Reihenfolge
+     * der Entries passen
+     */
+    private ArrayList<String> getLabels() {
+        // creating labels
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Task 1");
+        labels.add("Task 2");
+        labels.add("Task 3");
+        return labels;
+    }
+}
--- app/src/main/java/de/hdm_stuttgart/zeitfresser/MainActivity.java
+++ app/src/main/java/de/hdm_stuttgart/zeitfresser/MainActivity.java
@@ -1,13 +1,95 @@
 package de.hdm_stuttgart.zeitfresser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+/**
+ * MainActivity ist die Aktivität, die beim Start der App bzw. beim Klick auf den Eintrag
+ * "Data input" im Drawer-Menü aufgerufen wird
+ */
+public class MainActivity extends CommonActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /**
+         * Nach dem Start der App wird die Ansicht angezeigt, die in der Ressource
+         * activity_main.xml definiert ist
+         */
         setContentView(R.layout.activity_main);
+
+        /**
+         * Hier wird das Drawer-Menü aufgebaut
+         * Dieser Aufruf muss im Rahmen der Vorlesung nicht geändert werden
+         */
+        createNavigationBars();
+
+
+        /**
+         * Hier wird die Liste der Tätigkeiten aufgebaut. listview ist die Referenz auf das
+         * Listenelement, das die Namen der Tätigkeiten enthält
+         */
+        final ListView listview = (ListView) findViewById(R.id.listView);
+
+        /**
+         * Aufruf der Methode, welche die Inhalte der dargestellten Liste liefert
+         */
+        final ArrayList<String> list = getListElements();
+
+        /**
+         * Der Adapter bildet die Elememnte aus der Liste "list" auf Einträge des Listen-Widgets
+         * in der GUI ab
+         */
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        /**
+         * Hier ist der Listener definiert, der auf Benutzerklicks reagiert
+         */
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                /**
+                 * Abfrage, welches Listenelement geklickt wurde. "item" enthält den String, der
+                 * im Listenelement dargestellt wird
+                 */
+                final String item = (String) parent.getItemAtPosition(position);
+
+                /**
+                 * TODO: hier ist die anwendungsspezifische Logik zu implementieren:
+                 * Momentan wird als Reaktion auf den Benutzerklick ein "Toast"
+                 * (Benachrichtigungsfenster) für die Dauer "duration" angezeigt, in dem
+                 * als Text das angeklickte Element (item) steht
+                 */
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(getApplicationContext(), item, duration);
+                toast.show();
+            }
+
+        });
+    }
+
+    /**
+     * TODO: diese Methode muss anwendungsspezifisch implementiert werden. Momentan liefert sie
+     * hard kodierte Namen von Tätigkeiten. Im ersten Schritt der Anwendung sollen diese Namen aus
+     * weiteren Objekten, z.B. Instanzen einer Klasse Task, ausgelesen und hier zu einer ArrayList
+     * zusammengebaut werden
+     */
+    private ArrayList<String> getListElements() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Task 1");
+        list.add("Task 2");
+        list.add("Task 3");
+        return list;
     }
 }
