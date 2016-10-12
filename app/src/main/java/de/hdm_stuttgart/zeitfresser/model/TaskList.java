83f95f55ea409edf901f825ecd2ea885133fdc47
Simon Lipke <simon.lipke@googlemail.com>
2016-10-12 17:09:14 +0200

    Added TaskList

---
+++ app/src/main/java/de/hdm_stuttgart/zeitfresser/model/TaskList.java
@@ -0,0 +1,38 @@
+package de.hdm_stuttgart.zeitfresser.model;
+
+import java.util.ArrayList;
+
+public class TaskList {
+
+    public TaskList() {
+
+    }
+
+    public void createTaskList() {
+
+    }
+
+    public void addTask(String name, long id) {
+
+    }
+
+    public ArrayList<String> getAllNames() {
+
+    }
+
+    public void setTaskActive(String name) {
+
+    }
+
+    public void setTaskInactive(String name) {
+
+    }
+
+    public boolean isTaskActive(String name) {
+
+    }
+
+    public Task getTaskForName(String name) {
+
+    }
+}
