42eade044c875551d717f4068e4395cbe50232ea
Simon Lipke <simon.lipke@googlemail.com>
2016-10-12 17:01:39 +0200

    Added model

---
+++ app/src/main/java/de/hdm_stuttgart/zeitfresser/model/Record.java
@@ -0,0 +1,55 @@
+package de.hdm_stuttgart.zeitfresser.model;
+
+import java.util.Date;
+
+class Record {
+
+    private long id;
+    private long duration;
+    private Date start;
+    private Date end;
+
+    public Record() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public Date getStartTime() {
+        return start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public Date getEndTime() {
+        return end;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}
---
+++ app/src/main/java/de/hdm_stuttgart/zeitfresser/model/Task.java
@@ -0,0 +1,49 @@
+package de.hdm_stuttgart.zeitfresser.model;
+
+import java.util.ArrayList;
+
+class Task {
+
+    private long id;
+    private boolean on;
+    private String name;
+    private ArrayList<Record> records;
+    private Record activeRecord;
+
+    public Task(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean isOn() {
+        return this.on;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public ArrayList<Record> getRecords() {
+        return this.records;
+    }
+
+    public void setRecords(ArrayList<Record> records) {
+        this.records = records;
+    }
+
+    public void setActiveRecord(Record record) {
+        this.activeRecord = record;
+    }
+
+    public Record getActiveRecord() {
+        return this.activeRecord;
+    }
+}
