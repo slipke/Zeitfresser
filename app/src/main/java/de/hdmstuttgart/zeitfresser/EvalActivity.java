bb5603f4a9dfbc2fca5cf1de22d4d2975afad1e5
bh051 <bh051@hdm-stuttgart.de>
2016-12-12 19:35:57 +0100

    extended gui with date input fields and date pickers (evaluation view)

--- app/src/main/java/de/hdmstuttgart/zeitfresser/EvalActivity.java
+++ app/src/main/java/de/hdmstuttgart/zeitfresser/EvalActivity.java
@@ -1,6 +1,13 @@
 package de.hdmstuttgart.zeitfresser;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
@@ -8,6 +15,7 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.Calendar;
 import java.util.List;
 
 
@@ -17,6 +25,11 @@ import java.util.List;
  */
 public class EvalActivity extends CommonActivity {
 
+  private DatePickerDialog fromDatePicker;
+  private DatePickerDialog toDatePicker;
+
+  private EditText fromEditText;
+  private EditText toEditText;
   /**
    * diese Methode muss nicht verändret werden, sie baut das Kuchendiagramm auf.
    */
@@ -38,8 +51,60 @@ public class EvalActivity extends CommonActivity {
     dataset.setColors(ColorTemplate.COLORFUL_COLORS); // set the color
     dataset.setValueTextSize(16);
 
+    initDatePickerDialogs();
   }
 
+  private void initDatePickerDialogs(){
+    Calendar calendar = Calendar.getInstance();
+
+    // implement from date picker dialog
+    fromDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+      @Override
+      public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        //TODO: implement logic here
+        month++; // month counting begins at 0 - strange
+        Toast.makeText(getApplicationContext(), "set from date: "+day+"."+month+"."+year, Toast
+                .LENGTH_LONG).show();;
+        fromEditText.setText(day+"."+month+"."+year);
+      }
+    },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+
+    // implement to date picker dialog
+    toDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+      @Override
+      public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        //TODO: implement logic here
+        month++;
+        Toast.makeText(getApplicationContext(), "set to date: "+day+"."+month+"."+year, Toast
+                .LENGTH_LONG).show();;
+        toEditText.setText(day+"."+month+"."+year);
+      }
+    },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+
+    // get date edit text fields
+    fromEditText = (EditText) findViewById(R.id.fromDateEditText);
+    toEditText = (EditText) findViewById(R.id.toDateEditText);
+
+    // prevent showing keyboard
+    fromEditText.setInputType(InputType.TYPE_NULL);
+    // register from edit text listener
+    fromEditText.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        fromDatePicker.show();
+      }
+    });
+
+    // prevent showing keyboard
+    toEditText.setInputType(InputType.TYPE_NULL);
+    // register to edit text listener
+    toEditText.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        toDatePicker.show();
+      }
+    });
+  }
   /**
    * TODO: diese Methode muss anwendungsspezifisch überschrieben werden
    * Sie liefert eine Liste von Zahlen, die im Kuchendiagramm angezeigt werden
