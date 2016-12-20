5b3541357460031130446f715d14e0d567f52872
patrick.kleindienst <patrick.kleindienst@web.de>
2016-12-20 12:42:21 +0100

    Added state tests for checking 1) initial state, 2) state after start() and 3) state after double-start. This addresses [Issue #16] but does not finally fix it since more tests have to be implemented.

--- app/src/test/java/de/hdmstuttgart/zeitfresser/model/TaskTest.java
+++ app/src/test/java/de/hdmstuttgart/zeitfresser/model/TaskTest.java
@@ -1,18 +1,18 @@
 package de.hdmstuttgart.zeitfresser.model;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.List;
 
-
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 
 
 /**
@@ -26,10 +26,104 @@ public class TaskTest {
 
   private final int durationOffset = 1000;
 
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
   @Before
   public void setUp() throws Exception {
     classUnderTest = Task.withName("DummyTask");
 
+  }
+
+
+  /**
+   * If a new task instance has been created, it is expected to be in the following original state:
+   * <br/>
+   * <ul>
+   * <li>Its record list must be empty.</li>
+   * <li>It is not in active state.</li>
+   * <li>It does not have an active record.</li>
+   * <li>It has a valid name different from <em>null</em>.</li>
+   * </ul>
+   */
+  @Test
+  public void testInitialTaskState() {
+    assertThat("Newly created task must not be in active state!",
+        classUnderTest.isActive(), equalTo(false));
+    assertThat("Newly created task must not have any records!",
+        classUnderTest.hasAnyRecords(), equalTo(false));
+    assertThat("Newly created task must not have an active record!",
+        classUnderTest.hasActiveRecord(), equalTo(false));
+    assertThat("Task name must not be null!", classUnderTest.getName(), notNullValue());
+  }
+
+
+  /**
+   * After a newly created task has been started, it is expected to be in following state:
+   * <br/>
+   * <ul>
+   * <li>It must be in active state.</li>
+   * <li>Its record list must not be empty and contain exactly one element.</li>
+   * <li>It must have an active record.</li>
+   * <li>The record in the list must be the same as the active record.</li>
+   * </ul>
+   */
+  @Test
+  public void testStateAfterStartingNewlyCreatedTask() throws Exception {
+    classUnderTest.start();
+
+    Field recordsField = getFieldFromTestClass("records");
+    List<Record> records = (List<Record>) recordsField.get(classUnderTest);
+
+    Field activeRecordField = getFieldFromTestClass("activeRecord");
+    Record activeRecord = (Record) activeRecordField.get(classUnderTest);
+
+    assertThat("Task must be in active state!", classUnderTest.isActive(), equalTo(true));
+    assertThat("Task's record list must not be empty!",
+        classUnderTest.hasAnyRecords(), equalTo(true));
+    assertThat("Record list must contain exactly one element!", records.size(), equalTo(1));
+    assertThat("Task must have an active record!", classUnderTest.hasActiveRecord(), equalTo(true));
+    assertThat("Active record must be present in records list!",
+        records.contains(activeRecord), equalTo(true));
+  }
+
+
+  /**
+   * In case an already activated task gets started a second time, an
+   * {@link IllegalStateException} is expected to be thrown. Furthermore, the double-activation
+   * shall have no undesired side-effects as far as the task's state is concerned. That means
+   * that the task is expected to be in following state after being stated twice:
+   * <br/>
+   * <ul>
+   * <li>It must be in active state.</li>
+   * <li>Its record list must not be empty and contain exactly one element.</li>
+   * <li>It must have an active record.</li>
+   * <li>The record in the list must be the same as the active record.</li>
+   * </ul>
+   */
+  @Test
+  public void testStartAlreadyActiveTaskThrowsException() throws Exception {
+    expectedException.expect(IllegalStateException.class);
+    expectedException.expectMessage("Task has already been started!");
+
+    classUnderTest.start();
+    classUnderTest.start();
+
+    Field recordsField = getFieldFromTestClass("records");
+    List<Record> records = (List<Record>) recordsField.get(classUnderTest);
+
+    Field activeRecordField = getFieldFromTestClass("activeRecord");
+    Record activeRecord = (Record) activeRecordField.get(classUnderTest);
+
+    assertThat("Double-activated task must still be active!",
+        classUnderTest.isActive(), equalTo(true));
+    assertThat("Double-activated task must still have records!",
+        classUnderTest.hasAnyRecords(), equalTo(true));
+    assertThat("Record list must still contain a single element!", records.size(), equalTo(1));
+    assertThat("Double-activated task must still have an active record!",
+        classUnderTest.hasActiveRecord(), equalTo(true));
+    assertThat("Active record must still be present in record list!",
+        records.contains(activeRecord), equalTo(true));
   }
 
   @Test
