b5fd83ca2af67e052896aefa6f99b300e1a3c73a
patrick.kleindienst <patrick.kleindienst@web.de>
2017-02-15 09:29:47 +0100

    Fixed several Checkstyle warnings: Removed unused imports, fixed parentheses position, fixed '+' operator position in String concatenation.

--- app/src/androidTest/java/de/hdmstuttgart/zeitfresser/EvalActivityTest.java
+++ app/src/androidTest/java/de/hdmstuttgart/zeitfresser/EvalActivityTest.java
@@ -1,11 +1,16 @@
 package de.hdmstuttgart.zeitfresser;
 
-import android.app.DatePickerDialog;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.contrib.PickerActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
 import android.widget.DatePicker;
 
 import org.hamcrest.Matchers;
@@ -14,25 +19,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
 @RunWith(AndroidJUnit4.class)
 public class EvalActivityTest {
 
   @Rule
   public ActivityTestRule<EvalActivity> evalActivity =
-          new ActivityTestRule<>(EvalActivity.class);
+      new ActivityTestRule<>(EvalActivity.class);
 
   @Before
-  public void setUp(){
+  public void setUp() {
     //onView(withContentDescription("Open navigation drawer")).perform(click());
     //onView(withText(R.string.eval_activity)).perform(click());
   }
@@ -50,13 +45,14 @@ public class EvalActivityTest {
     int month = 1;
     int day = 10;
     onView(withId(R.id.fromDateEditText)).perform(click());
-    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check
-            (ViewAssertions.matches(isDisplayed()));
-    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform
-            (PickerActions.setDate(2017, 1, 10));
+    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(
+        ViewAssertions.matches(isDisplayed()));
+    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(
+        PickerActions.setDate(2017, 1, 10));
     onView(withText("OK")).perform(click());
-    onView(withId(R.id.fromDateEditText)).check(ViewAssertions.matches(withText(day+"."+month+"."
-            +year)));
+    onView(withId(R.id.fromDateEditText)).check(ViewAssertions.matches(withText(day + "." + month
+        + "."
+        + year)));
     //onView(withId(R.id.fromDateEditText)).perform(typeText("test text"));
   }
 
@@ -66,12 +62,12 @@ public class EvalActivityTest {
     int month = 2;
     int day = 24;
     onView(withId(R.id.toDateEditText)).perform(click());
-    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check
-            (ViewAssertions.matches(isDisplayed()));
-    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform
-            (PickerActions.setDate(year, month, day));
+    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(
+        ViewAssertions.matches(isDisplayed()));
+    onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(
+        PickerActions.setDate(year, month, day));
     onView(withText("OK")).perform(click());
-    onView(withId(R.id.toDateEditText)).check(ViewAssertions.matches(withText(day+"."+month+"."
-            +year)));
+    onView(withId(R.id.toDateEditText)).check(ViewAssertions.matches(withText(day + "."
+        + month + "." + year)));
   }
 }
