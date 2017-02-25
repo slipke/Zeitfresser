6aff7cb601bf00f779a7f6b20d408ea97a29d24b
patrick.kleindienst <patrick.kleindienst@web.de>
2017-02-25 20:59:06 +0100

    Introduced test suite for all test classes covering class "Record".

---
+++ app/src/test/java/de/hdmstuttgart/zeitfresser/model/RecordTestSuite.java
@@ -0,0 +1,13 @@
+package de.hdmstuttgart.zeitfresser.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by patrick on 25.02.17.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({RecordTest.class, RecordEqualsHashcodeTest.class})
+public class RecordTestSuite {
+}
